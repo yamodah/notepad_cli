bfbf6259aa16b3b41efcfb691e7abd3b73ee3440
yamodah <yassinedahlek@gmail.com>
2021-12-11 14:04:58 -0500

    completed and removed api url to keep privacy

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,17 @@
+#!/usr/bin/env node
+require("dotenv").config()
 const yargs = require("yargs")
-const url =  process.env.BASE_API_URL || "http://localhost:5000"
+const axios = require("axios").default
+
 
 const options = yargs
 .usage("Usage: -t <title> -c <content>")
 .option("t",{ alias:"title", describe:"Note title", type:"string", demandOption:true })
-.option("c",{alias:"content", describe:"Note content", type:"string", demandOption:true })
-.argv
+.option("c",{ alias:"content", describe:"Note content", type:"string", demandOption:true })
+.argv;
+
+const newNote = {
+    title:options.title,
+    content:options.content,
+}
+axios.post("",newNote).catch(console.error)
